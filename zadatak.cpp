9c43b7fd544d29eac66b581c8e61040a85814f3f
Veja08 <145063614+Veja08@users.noreply.github.com>
2025-05-06 10:59:22 +0200

    priority_queue
    blaga optimizacija sa priority queue

--- zadatak.cpp
+++ zadatak.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <algorithm>
 #include <iomanip> 
+#include <queue>
 
 using namespace std;
 
@@ -11,18 +12,18 @@ struct programer{
     double odnos;
 };
 
-double sortiraj(const vector<programer> &a, int pocetak, int kraj){
+double sortiraj(const vector<programer> &a, int pocetak, int kraj){ //Trazimo najveci odnos od i do k i vracamo taj odnos
     double max = 0;
     for(int i = pocetak; i <= kraj; i++){
         if (a[i].odnos > max)max = a[i].odnos;
     }
-    //cout << "Max je: " << max << '\n';
+    
     return max;
 }
 
 int main(){
 
-    int n; cin >> n;
+    int n; cin >> n; //Broj prijavljenih
     vector<programer>a(n);
     
     for(int i = 0; i < n; i++){
@@ -30,37 +31,22 @@ int main(){
         a[i].kval = k; a[i].plata = p; a[i].odnos = (double)p / k;
     }
 
-    int k; cin >> k;
+    int k; cin >> k; //Broj koji firma zeli da zaposli
     
-    sort(a.begin(), a.end(), [](programer b, programer c){
+    sort(a.begin(), a.end(), [](programer b, programer c){ //Sortiramo po rastucem odnosu
         return b.odnos < c.odnos;
     });
 
-    //for(int i = 0; i < a.size(); i++)
-       // cout << fixed << setprecision(2) << a[i].odnos << " ";
+    priority_queue<double, vector<double>, greater<double>> pq; //Pravimo red sa prioritetom koji ce redjati po rastucem redosledu
+    double sum = 0;
 
-    //cout << "\n";
-
-    vector<double>b(n, 0);
     for(int i = 0; i <= n-k; i++){
-        for(int j = i; j < i+k; j++){b[i] += a[j].kval;}
-        //cout << "B[i] je: " << b[i] << '\n';
-        //cout << "Max je: " << sortiraj(a, i, i+(k-1)) << '\n';
-        b[i] = b[i] * sortiraj(a, i, i+(k-1));
-        //cout << "B[i] je: " << b[i] << '\n';
-        //if (b[i] == 0)b.erase(b.begin() + i);
+        for(int j = i; j < i+k; j++){sum += a[j].kval;} //Racunamo sumu kvaliteta od i do k
+        sum = sum * sortiraj(a, i, i+(k-1)); //Mnozimo sumu sa najveci odnos od programera izmedju i i k pozicije
+        pq.push(sum); sum = 0; //Ubacujemo konacnu sumu u red sa prioritetom i vracamo sumu na 0 za sledece racunanje
     }
 
+    cout << fixed << setprecision(2) << pq.top(); //Ispisujemo vrh pq-a koji sadrzi nase konacno resenje
 
-    //make_heap(b.begin(), b.end());
-    //sort_heap(b.begin(), b.begin() + k);
-    sort(b.begin(), b.begin() + k);
-
-    //while(b[0] == 0)b.erase(b.begin());    
-
-    //cout << "B vektor: " << '\n';
-    for(int i = 0; i < b.size(); i++)
-        cout << fixed << setprecision(2) << b[i] << " ";
-    //cout << fixed << setprecision(2) << b[0];
 
 }
